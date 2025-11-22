7180878c28a1306b8f66491767030c460c3ed4df
GHOST <tasinhossainorko178867@yahoo.com>
2025-11-23 03:26:01 +0600

    Append

--- Python_CWH/09. File IO - Working with Files & Related Modules/2. Read, Write, and Append Files.py
+++ Python_CWH/09. File IO - Working with Files & Related Modules/2. Read, Write, and Append Files.py
@@ -1,12 +1,23 @@
-try:
-    f = open("Read.txt", "r")
-    content = f.read()
-    print(content)
-    f.close()
+# Read
+# f = open("Read.txt", "r")
+# content = f.read()
+# print(content)
+# f.close()
 
-except FileNotFoundError:
-    print("File not found.")
+# Write
+# f = open("GHOST.txt", "w")
+# string = '''
+# Ghost is a very nice guy. He lives in Milan and he works with.
+# His favourite is numpy
+# He is a ghost
+# '''
+# f.write(string)
+# f.close()
 
-
-
-
+# Append
+f = open("GHOST.txt", "a")
+string = '''
+Ghost is a CSE student at AIUB
+'''
+f.write(string)
+f.close()
