0d24e142e80ca90f2652d55e939e0b9eeaad08e6
GHOST <tasinhossainorko178867@yahoo.com>
2025-11-23 03:34:18 +0600

    Write

--- Python_CWH/09. File IO - Working with Files & Related Modules/2. Read, Write, and Append Files.py
+++ Python_CWH/09. File IO - Working with Files & Related Modules/2. Read, Write, and Append Files.py
@@ -15,9 +15,24 @@
 # f.close()
 
 # Append
-f = open("GHOST.txt", "a")
-string = '''
-Ghost is a CSE student at AIUB
-'''
-f.write(string)
-f.close()
+# f = open("GHOST.txt", "a")
+# string = '''
+# Ghost is a CSE student at AIUB
+# '''
+# f.write(string)
+# f.close()
+
+# Line By Line
+# f = open("Ghost.txt","r" )
+# for  line in f:
+#     print(line)
+# f.close()
+
+# With
+# f = open("Read.txt", "r")
+# # content = f.read()
+# print(f.read())
+# f.close()
+with open("Read.txt", "r") as f: #Context manager
+    content = f.read()
+    print(content)
